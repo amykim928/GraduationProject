f937655c0930ef86002c3281b87b23e3acc0519b
jj-hyunee <jj.hyun52.6@gmail.com>
2022-05-29 13:53:53 +0900

    add all functions

--- app/src/main/java/com/example/closet_app/RecommendActivity.kt
+++ app/src/main/java/com/example/closet_app/RecommendActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
 
 class RecommendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,33 @@ class RecommendActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recommend)
 
         val gohome: Button = findViewById<View>(R.id.gohome) as Button
+
+        val gSatisfaction: RadioGroup = findViewById<View>(R.id.gSatisfaction) as RadioGroup
+        val saveSatisfaction: Button = findViewById<View>(R.id.saveSatisfaction) as Button
+        val saveResult: Button = findViewById<View>(R.id.saveResult) as Button
+        val retry: Button = findViewById<View>(R.id.retry) as Button
+        val goCloset: Button = findViewById<View>(R.id.goCloset) as Button
+
+        saveSatisfaction.setOnClickListener{
+            Toast.makeText(this, "만족도를 저장했습니다!", Toast.LENGTH_SHORT).show()
+            gSatisfaction.clearCheck()
+        }
+
+        saveResult.setOnClickListener {
+            //val myIntent = Intent(this, ClosetActivity::class.java)
+            Toast.makeText(this, "결과를 저장했습니다!", Toast.LENGTH_SHORT).show()
+        }
+
+        retry.setOnClickListener {
+            //val myIntent = Intent(this, ClosetActivity::class.java)
+            //startActivity(myIntent)
+        }
+
+        goCloset.setOnClickListener {
+            val myIntent = Intent(this, ClosetActivity::class.java)
+            startActivity(myIntent)
+        }
+
         gohome.setOnClickListener {
             val myIntent = Intent(this, MainActivity::class.java)
             startActivity(myIntent)

55a40017cc69b41f1a1640be5f8d7ab017ab7aeb
Spstars <gijin100@naver.com>
2022-06-01 04:35:43 +0900

    "try to mk APIs"

--- app/src/main/java/com/example/closet_app/data/API.kt
+++ app/src/main/java/com/example/closet_app/data/API.kt
@@ -1,13 +1,14 @@
 package com.example.closet_app.data
 
 import com.google.gson.JsonObject
-import retrofit2.http.GET
+
 import retrofit2.Call
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface API {
     interface RetrofitAPI {
         @POST("/predict2")//서버에 GET요청을 할 주소를 입력
-        fun postPredict() : Call<JsonObject> //MainActivity에서 사용할 json파일 가져오는 메서드
+        fun postPredict(@Query("Json") jsonObject: JsonObject) : Call<JsonObject> //MainActivity에서 사용할 json파일 가져오는 메서드
     }
 }
